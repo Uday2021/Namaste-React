a8e387231c422ead60ec8b3107270e1355104292
udaysaraswat <udaysaraswat12@gmail.com>
2023-07-30 21:08:20 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,22 @@
+/** <div id="parent">
+ * <div>
+ *   <h1>Hello world</h1>
+ * </div>
+ *</div>;
+ *
+ * ReactElement(object) => HTML(browser understands)
+ */
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Hello world reactjs h1"),
+    React.createElement("h2", {}, "Hello world reactjs h2"),
+  ])
+);
+console.log(parent); //object
+
+// const heading = React.createElement("h1", { id: "heading" }, "Hello world!");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
